e21e7de31d9ddd1c7256fe735a86a37690e7f15a
MadWeenie <maxim2001357@yandex.ru>
2017-05-09 16:55:23 +0300

    Work with Names

---
+++ src/main/java/JavaTask1L7/TwoStrings.java
@@ -0,0 +1,20 @@
+package JavaTask1L7;
+
+public class TwoStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "IT1"; 
+		String s2 = "IT Academy";
+		if (s2.contains(s1)) {
+			System.out.println(s2 +" contains " + s1);
+		}
+			else
+			{
+				System.out.println(s2 +" doesn't contains " + s1);
+			}
+	}
+
+}
+
+
