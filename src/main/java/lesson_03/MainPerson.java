eabb2fb532a22e72c4717d118693315b17830210
Pavlo Kroitor <pkroitc@training.local>
2017-05-11 19:46:46 +0300

    first commit

---
+++ src/main/java/lesson_03/MainPerson.java
@@ -0,0 +1,33 @@
+package lesson_03;
+
+import java.util.ArrayList;
+public class MainPerson {
+
+    public static void main(String[] args) {
+        ArrayList<Person> personList = new ArrayList<Person>();
+
+        Person p1 = new Person("Bob", 1987);
+        personList.add(p1);
+        Person p2 = new Person("Tomas", 1967);
+        personList.add(p2);
+        Person p3 = new Person("Jeck", 1993);
+        personList.add(p3);
+        Person p4 = new Person("Tony", 2002);
+        personList.add(p4);
+        Person p5 = new Person("Sem", 1982);
+        personList.add(p5);
+
+        for (Person person: personList){
+            System.out.println(person);
+            person.age();
+            person.changeName("Roman");
+        }
+
+		/*System.out.println(p1);
+		p1.age();
+		p1.changeName("Roman");
+		p1.input();*/
+        //p1.output();
+
+    }
+}
---
+++ src/main/java/lesson_03/Person.java
@@ -0,0 +1,55 @@
+package lesson_03;
+
+import java.util.Scanner;
+
+public class Person {
+
+    private String name;
+    private int birthYear;
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public int getYear() {return birthYear;}
+
+    public void setYear(int year) {this.birthYear = year;}
+
+    public Person(){
+    }
+
+    public Person(String name, int year){
+        this.name = name;
+        this.birthYear = year;
+    }
+    @Override
+    public String toString() {
+        return "Person name is " + getName() + ", birthYear is " + getYear() + ".";
+    }
+
+    public void age(){
+        int age = 2017 - birthYear;
+        System.out.println(getName() + " age is " + age);
+    }
+
+    public int input(){
+        Scanner sc = new Scanner(System.in);
+        int growth;
+        int weight;
+        System.out.println("Enter growth for " + getName());
+        growth = sc.nextInt();
+        System.out.println("Enter wight for " + getName());
+        weight = sc.nextInt();
+        sc.close();
+        return growth + weight;
+    }
+
+    public void output(){
+        System.out.println("Person " + getName() + " growth and weight is " + input());
+    }
+
+    public void changeName(String name){
+        System.out.println("New name for " + getName() + " is " + name);
+    }
+}
+
