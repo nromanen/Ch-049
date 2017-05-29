1c2eef0389684d819febaeb42d5af175635e14f3
kidpres <kidpres666@gmail.com>
2017-05-29 10:42:48 +0300

    for git

--- eightLtask/PT2PlantsMain.java
+++ eightLtask/PT2PlantsMain.java
@@ -6,7 +6,21 @@ import java.util.List;
 public class PT2PlantsMain {
 
 	public static void main(String[] args) {
-		
+			
+		try {
+			PlantPT2[] plants = new PlantPT2[5];
+			plants[0] = new PlantPT2(3, "red", "oak");
+			plants[1] = new PlantPT2(3, "red", "oak1");
+			plants[2] = new PlantPT2(3, "pink", "oak");
+			plants[3] = new PlantPT2(3, "pink", "oak");
+			plants[4] = new PlantPT2(3, "pink", "oak");
+		} catch (ColorExeption e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TypeExeption e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 
 	}
